6dc180781b0b5767741f06a6d216fc5b8af500a6
sunjm <sunjm@SUN>
2025-07-18 23:23:46 +0800

    initial commit

---
+++ MavenProject/src/main/java/Example.java
@@ -0,0 +1,9 @@
+
+public class Example {
+	private static final String MESSAGE = "Hello World!";
+      
+    public String getMessage() {
+        return MESSAGE;
+    }
+
+}
---
+++ MavenProject/src/test/java/TestExample.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestExample {
+
+	 @Test
+	    public void testAppMessage()
+	    {
+	        Example example = new Example();
+	        assertEquals("Hello World!", example.getMessage());
+	        assertEquals("Hello World!", example.getMessage());
+	    }
+
+}
